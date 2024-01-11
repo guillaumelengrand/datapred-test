9631dc19bbe2a3f7a04e4cc05b1f52d08517778d
Guillaume Lengrand <guillaume.lengrand@gmail.com>
2024-01-11 14:10:29 +0100

    Feat: add datepicker and first fetch to find if a run exist at the date choose

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+    // State
+    const [msg, setMsg] = useState(null);
+
+    // Methods
+    const fetchFromDate = async date => {
+        let isoDate = new Date(date).toISOString().replace('.000', '');
+
+        try {
+            const fetchRuns = await fetch(
+                `https://test-backend.i.datapred.com/without-auth/flows/1/runs?production_date=${isoDate}`,
+                {
+                    headers: {
+                        Accept: 'application/json',
+                        'Content-Type': 'application/json',
+                    },
+                    method: 'GET',
+                },
+            );
+            if (fetchRuns.ok) {
+                console.log('Run find');
+                setMsg(null);
+            } else {
+                setMsg('No run available for this date');
+            }
+        } catch (error) {
+            console.log({error});
+        }
+    };
     return (
         <div className="App">
             <div className="App-header">
                 <div>Choose a date:</div>
+                <input type="date" onChange={e => fetchFromDate(e.currentTarget.value)} />
+                {msg && <div>{msg}</div>}
             </div>
         </div>
     );
