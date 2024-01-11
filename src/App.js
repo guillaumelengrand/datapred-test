6df8f0ee0ebad64731613fbdda2a28564b4833f1
Guillaume Lengrand <guillaume.lengrand@gmail.com>
2024-01-11 13:59:36 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <div className="App-header">
+                <div>Choose a date:</div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
